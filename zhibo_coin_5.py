83a9e5f3225c3ed68eaeb49fd82372cdb8962a95
李献涛 <justin.li@ankobot.com>
2020-04-20 14:09:12 +0800

    修复时间范围问题

--- zhibo_coin_5.py
+++ zhibo_coin_5.py
@@ -56,18 +56,20 @@ def cycle():
     """
     while True:
         currentTimestamp = int(time.time())
-        hourTimestampFormat = time.strftime("%Y-%m-%d %H:00:00", time.localtime())
+        hourTimestampFormat = time.strftime("%Y-%m-%d 00:00:00", time.localtime())
         hourTimestampArray = time.strptime(hourTimestampFormat, "%Y-%m-%d %H:%M:%S")
+        hourTimestamp = int(time.mktime(hourTimestampArray) + 30)
         # 开始时间点
         startLoopPoint = time.strftime("%Y-%m-%d 23:59:55", time.localtime())
         startLoopPointArray = time.strptime(startLoopPoint, "%Y-%m-%d %H:%M:%S")
         startLoopPointTimestamp = int(time.mktime(startLoopPointArray))
         # print(startLoopPointTimestamp)
-        hourTimestamp = int(time.mktime(hourTimestampArray) + 30)
         # 当前时间在 T 23:59:55 --- T+1 00:00:30 之间
-        if currentTimestamp > startLoopPointTimestamp and currentTimestamp < hourTimestamp:
-            exchange()
-            time.sleep(1)
+        if currentTimestamp < startLoopPointTimestamp or currentTimestamp > hourTimestamp:
+            continue
+
+        exchange()
+        time.sleep(1)
 
 def main():
     # 调用
