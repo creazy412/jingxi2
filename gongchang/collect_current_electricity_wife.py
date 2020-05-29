2ff3a155d2e65e74703ab669edebfa00588d0b3e
李献涛 <justin.li@ankobot.com>
2020-05-29 18:54:45 +0800

    fix token invalid problem

--- gongchang/collect_current_electricity_wife.py
+++ gongchang/collect_current_electricity_wife.py
@@ -10,14 +10,14 @@ def collect_electric():
 
     headers = {
         'authority': 'wq.jd.com',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        'sec-fetch-dest': 'script',
         'accept': '*/*',
         'sec-fetch-site': 'same-site',
         'sec-fetch-mode': 'no-cors',
-        'sec-fetch-dest': 'script',
-        'referer': 'https://wqs.jd.com/pingou/dream_factory/index.html?share_ptag=1.1.1&jxsid=15902028949811582480&PTAG=17053.57.1&_fromplatform=jxapp&ad_od=share&utm_source=androidpingouapp&utm_medium=pingouappshare&utm_campaign=t_335139774&utm_term=CopyURL',
+        'referer': 'https://wqs.jd.com/pingou/dream_factory/index.html?share_ptag=1.1.1&jxsid=15907492306869524270&PTAG=17053.57.1&_fromplatform=jxapp&ad_od=share&utm_source=androidpingouapp&utm_medium=pingouappshare&utm_campaign=t_335139774&utm_term=CopyURL',
         'accept-language': 'zh-CN,zh;q=0.9',
-        'cookie': '__jdu=1589984592067854966288; 3AB9D23F7A4B3C9B=ZJB2J3YFRYGTLIRTEHG6SVLYHZHNUUKLVDWFM252ROKFFG7PCA2AJFZU3B7QOBIUOUZJP43PLUO7WUIHVUOZFW5G3Q; pinId=p9q4GZ0bvhUviOw3JTn08g; unick=%E7%8C%AE%E6%B6%9B%E5%8B%87; _tp=%2BWZS%2FWDHCOCSXWgN63a%2BSw%3D%3D; _pst=754634469_m; TrackID=1WIDrOcd4czpv5wNL5oyECQhYUCZ2irsS_BRGqhqBz7DaMTptSRFug19fd3inoGVbGNzsxyI98Y9Um0a7rh9sqDz-SDrqq9LRcj3ayyvK-7g; cid=3; webp=1; sc_width=1536; visitkey=10913513581653556; PPRD_P=UUID.1589984592067854966288-EA.17053.57.1; shshshfpa=6549d940-e666-8194-e497-909ec30a88c4-1590202973; __jda=122270672.1589984592067854966288.1589984592.1590118760.1590202974.3; mba_muid=1589984592067854966288; shshshfpb=of%202NC3JqmU%20oH38eBcm4AA%3D%3D; rurl=https%3A%2F%2Fwqs.jd.com%2Fpingou%2Fdream_factory%2Findex.html%3Fshare_ptag%3D1.1.1%26jxsid%3D15902028949811582480%26PTAG%3D17053.57.1%26_fromplatform%3Djxapp%26ad_od%3Dshare%26utm_source%3Dandroidpingouapp%26utm_medium%3Dpingouappshare%26utm_campaign%3Dt_335139774%26utm_term%3DCopyURL; __jdc=122270672; __jd_ref_cls=MLoginRegister_RiskCheck; TrackerID=jfiBjViqqw7kCBKUlS8EUF9OYd89N7T5_JNUBaPy_J3BRy6RkJMxOq2KpdaVSyiz4KPi-OpiHISp4SliZh8ciUSI2522I6nppmikSBIb7EE; pt_key=AAJeyJMKADCVjMq5P52EUt58i2ZYB7OsxwaD0qhUUAP6dLopLy3Vzb_qhCTsq-8fJJIRShqUjZo; pt_token=s23xndda; pwdt_id=jd_478f44263fce3; pt_pin=jd_478f44263fce3; wxa_level=1; buy_uin=21620582635; jdpin=jd_478f44263fce3; pin=jd_478f44263fce3; wq_uin=21620582635; wq_skey=zm66F0EF2BB5A7615DDA1AC1829FC572E9E66A7600E0709ED4E359F1E4347284237719C7C82814D3E3C5F99FFD4D4DFB4C34427D74EA9B25F00237998BBA048090C56B6C47026FB0CEFB3F397342C06ED2BCB482FDEA56B0908479FE130CB28E4D; _tj_rvurl=https%3A//wq.jd.com/cube/front/activePublish/dream_factory_report/380556.html%3Fshare_ptag%3D1.1.1%26jxsid%3D15902028949811582480%26PTAG%3D17053.57.1%26_fromplatform%3Djxapp%26ad_od%3Dshare%26utm_source%3Dandroidpingouapp%26utm_medium%3Dpingouappshare%26utm_campaign%3Dt_335139774%26utm_term%3DCopyURL; wq_area=15_1213_0%7C3; retina=1; wqmnx1=MDEyNjM1MGg6c2NpL210aS4/ZWcuczUyODJQMS4mbWZqJmRybXJuZG8mbW1ncGVfYXQxNF89VTgzODV6Lyh4ZCAgc3VNTnBiNTZUbEcpbzEwM2JTaS41ZjduMjQyWU9PVSFIJQ%3D%3D; __wga=1590674699942.1590674597141.1590643924208.1590202971653.2.10; __jdv=122270672%7Candroidpingouapp%7Ct_335139774%7Cpingouappshare%7CCopyURL%7C1590674699966; shshshfp=017bccefda96b4ae7b6cb3b7e1a04232; shshshsID=03ec88f6d720ff180d2d037206867bbb_2_1590674700867',
+        'cookie': 'shshshfpa=490f7e17-1abe-d98b-e7ad-820b5f44f42f-1571622018; shshshfpb=khsT03U5oDfyZ%2FoU01w2Bfw%3D%3D; pinId=p9q4GZ0bvhUviOw3JTn08g; __jdu=1433937085; TrackID=1H6V0HWVPQH1CTcFqAalsPMA_2Yl6dGufZZX4pjjLKXbVyhbCwXEJDgDlvxINjb782x7OPHVMXmGjcinyVtp61E3oYjS7DgjtzNdfn8kT4Fo; user-key=c0a987f2-6776-4e56-a23c-e12f1e5331b5; cn=2; __jda=122270672.1433937085.1565602967.1588509394.1590669227.28; __jdc=122270672; areaId=19; ipLoc-djd=19-1607-4773-0; 3AB9D23F7A4B3C9B=HOENZJ47QVRFRK6WUOKPXZWRJX6DERJ4DJRRGLOXRJTI7NXJMCUYGHBIY54KEYZQ234HYRAQJLQ6DT6NPDPRA6BB4U; wxa_level=1; retina=1; cid=3; webp=1; sc_width=1536; wq_area=19_1607_4773%7C3; rurl=https%3A%2F%2Fwqs.jd.com%2Fpingou%2Fdream_factory%2Findex.html%3Fshare_ptag%3D1.1.1%26jxsid%3D15907492306869524270%26PTAG%3D17053.57.1%26_fromplatform%3Djxapp%26ad_od%3Dshare%26utm_source%3Dandroidpingouapp%26utm_medium%3Dpingouappshare%26utm_campaign%3Dt_335139774%26utm_term%3DCopyURL; TrackerID=xibJX-2qEsgmtuYUf8mKF5-2NSUzhKSlfsU0NqCQN8a5xucdEsEXzwQVaYs0jQlm2GDsHqYhmU2-pq-UVit_3scqUw6X4TdFMrfZYiqSa05UIgJk8FdfbkoAZNtxNigbMATQRMMGgpuZCVsAWqbAzg; pt_key=AAJe0OkkADCmGV6ULGx5E6DeNxuJgEfy8YyMtk3LrlMWRftYUh_XFk2_xcUC7oNYQortQ6m3Gr8; pt_pin=jd_478f44263fce3; pt_token=9wyop8ok; pwdt_id=jd_478f44263fce3; buy_uin=21620582635; jdpin=jd_478f44263fce3; pin=jd_478f44263fce3; wq_skey=zm66F0EF2BB5A7615DDA1AC1829FC572E923BFA271287DCAF1FD10163506A1450EB2010EFD4349A64B75A7D308C67CA98F34427D74EA9B25F00237998BBA048090D7982602B71A2597CE2331333FF370917FA9085E92B511C7B35B054AB64DDE0A; wq_uin=21620582635; wqmnx1=MDEyNjM5Ny9vL2NlaD1zNDVBNW09XyZjZHBpdSZhNXRvTS5laDNlWGUuVCBlLmwgMEZkLTJVKSY%3D; __wga=1590749478280.1590749350164.1590749350164.1590749350164.2.1; __jdv=76161171%7Candroidpingouapp%7Ct_335139774%7Cpingouappshare%7CCopyURL%7C1590749478287; shshshfp=bb28c4616e6e3e83d8e4e812ae8a4ea1; shshshsID=e778652691e786b70c1e3544b0de2ead_3_1590749478680; visitkey=68838765784605337; _tj_rvurl=https%3A//wq.jd.com/cube/front/activePublish/dream_factory_report/380556.html%3Fshare_ptag%3D1.1.1%26jxsid%3D15907492306869524270%26PTAG%3D17053.57.1%26_fromplatform%3Djxapp%26ad_od%3Dshare%26utm_source%3Dandroidpingouapp%26utm_medium%3Dpingouappshare%26utm_campaign%3Dt_335139774%26utm_term%3DCopyURL; PPRD_P=UUID.1433937085-EA.17053.57.1',
     }
 
 
@@ -25,6 +25,9 @@ def collect_electric():
     microsecond2 = int(round(t2 * 1000))
     params = (
         ('zone', 'dream_factory'),
+        ('apptoken', ''),
+        ('pgtimestamp', ''),
+        ('phoneID', ''),
         ('factoryid', '3306240'),
         ('doubleflag', '0'),
         ('timeStamp', 'undefined'),
@@ -32,7 +35,7 @@ def collect_electric():
         ('_', microsecond2),
         ('g_login_type', '0'),
         ('callback', 'jsonpCBKP'),
-        ('g_tk', '1058542785'),
+        ('g_tk', '810636292'),
         ('g_ty', 'ls'),
     )
 
